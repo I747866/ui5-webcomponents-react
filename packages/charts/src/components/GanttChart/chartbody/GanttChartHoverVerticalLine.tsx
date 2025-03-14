09e88f6cfed3d284c269d21bc915e07e15a99da8
I761284 <nathan.de.freitas@sap.com>
2025-03-14 11:00:26 +0100

    Add header text to hover line

--- packages/charts/src/components/GanttChart/chartbody/GanttChartHoverVerticalLine.tsx
+++ packages/charts/src/components/GanttChart/chartbody/GanttChartHoverVerticalLine.tsx
@@ -3,24 +3,48 @@ import React from 'react';
 
 interface GanttChartHoverVerticalLine {
   verticalLinePosition: number;
+  headerText: string;
 }
 
 /**
  * Component that renders a vertical line in the Gantt chart. This line is meant to be used as a hover line.
  */
-const GanttChartHoverVerticalLine: React.FC<GanttChartHoverVerticalLine> = ({ verticalLinePosition }) => {
+const GanttChartHoverVerticalLine: React.FC<GanttChartHoverVerticalLine> = ({ verticalLinePosition, headerText }) => {
+  const headerHeight = 28;
+
   return (
-    <div
-      style={{
-        position: 'absolute',
-        left: verticalLinePosition,
-        top: 0,
-        width: 1,
-        height: '100%',
-        backgroundColor: ThemingParameters.sapSelectedColor,
-        pointerEvents: 'none'
-      }}
-    />
+    <div style={{ position: 'absolute', left: verticalLinePosition, top: 0, height: '100%', pointerEvents: 'none' }}>
+      <div
+        style={{
+          position: 'absolute',
+          left: '50%',
+          transform: 'translateX(-50%)',
+          width: 'auto',
+          padding: '4px 8px',
+          marginTop: '10px',
+          backgroundColor: 'white',
+          border: `2px solid ${ThemingParameters.sapSelectedColor}`,
+          color: ThemingParameters.sapSelectedColor,
+          fontSize: '14px',
+          fontWeight: 'bold',
+          borderRadius: '4px',
+          whiteSpace: 'nowrap'
+        }}
+      >
+        {headerText}
+      </div>
+
+      <div
+        style={{
+          position: 'absolute',
+          left: '50%',
+          top: `${headerHeight + 10}px`,
+          width: '2px',
+          height: `calc(100% - ${headerHeight + 10}px)`,
+          backgroundColor: ThemingParameters.sapSelectedColor,
+        }}
+      />
+    </div>
   );
 };
 
